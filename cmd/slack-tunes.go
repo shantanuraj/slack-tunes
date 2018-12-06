d223add56d524f38de260cb322783862ab1e9c35
Shantanu Raj <s@sraj.me>
2018-12-06 11:06:14 +0530

    1.3.0

--- cmd/slack-tunes.go
+++ cmd/slack-tunes.go
@@ -20,7 +20,7 @@ type cleanupFn = func()
 
 const (
 	// AppVersion is the verison of the app
-	AppVersion = "1.2.0"
+	AppVersion = "1.3.0"
 	// AppName is the name of the app
 	AppName = "slack-tunes"
 	// AppUsage is the help text for the app
